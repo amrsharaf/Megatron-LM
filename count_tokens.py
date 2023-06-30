5d163b6bb8019d9c4ddc4e426b449f3e043c80a7
Amr Sharaf <amrsharaf@microsoft.com>
2023-06-29 21:17:28 -0700

    loop over all keys

--- count_tokens.py
+++ count_tokens.py
@@ -8,11 +8,11 @@ import json
 
 with open('tlgv5.2.0.json', 'r') as f:
     data = json.load(f)
-# print keys in a nice format
-#for key in data.keys():
-#data_prefix = key.replace('/mnt/stream/', '/turingnorwayeastpremium_data/')
-data_prefix = "/turingnorwayeastpremium_data/datasets/tnlgv4/binarized/cl100k_base/NIH_ExPorter_ftfy_id_text_Document"
-print_rank_0(' > data prefix: {}'.format(data_prefix))
-indexed_dataset = MMapIndexedDataset.Index(index_file_path(data_prefix), skip_warmup=True)
-tokens_per_epoch = _num_tokens(np.arange(indexed_dataset.sizes.shape[0]), indexed_dataset.sizes)
-print_rank_0(' > tokens per epoch: {:,}'.format(tokens_per_epoch))
+    # print keys in a nice format
+    for key in data.keys():
+        data_prefix = key.replace('/mnt/stream/', '/turingnorwayeastpremium_data/')
+#        data_prefix = "/turingnorwayeastpremium_data/datasets/tnlgv4/binarized/cl100k_base/NIH_ExPorter_ftfy_id_text_Document"
+        print_rank_0(' > data prefix: {}'.format(data_prefix))
+        indexed_dataset = MMapIndexedDataset.Index(index_file_path(data_prefix), skip_warmup=True)
+        tokens_per_epoch = _num_tokens(np.arange(indexed_dataset.sizes.shape[0]), indexed_dataset.sizes)
+        print_rank_0(' > tokens per epoch: {:,}'.format(tokens_per_epoch))
